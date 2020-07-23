5147018684e0a4870d896a411b4f931d6c9656f0
mpaolodr <mpaolodr_1504@yahoo.com>
2020-07-22 20:41:21 -0400

    bunny_ears 2

---
+++ bunny_ears2.py
@@ -0,0 +1,24 @@
+"""
+
+We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. 
+The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. 
+Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+"""
+
+
+def bunny_ears_var(n):
+
+    if n == 0:
+
+        return 0
+
+    if n % 2 == 0:
+
+        return 3 + bunny_ears_var(n - 1)
+
+    else:
+
+        return 2 + bunny_ears_var(n - 1)
+
+
+print(bunny_ears_var(2))
--- factorial.py
+++ factorial.py
@@ -1,3 +1,8 @@
+"""
+Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. 
+Compute the result recursively (without loops).
+"""
+
 
 def factorial(n):
 
